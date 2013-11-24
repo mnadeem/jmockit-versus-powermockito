89355f66f20d6eb1e93668309819f786c2c9ee0e
Nadeem Mohammad <coolmind182006@gmail.com>
2013-11-24 21:12:26 +0530

    add state based tests

--- src/test/java/com/nadeem/app/compare/mock/jmockit/case1/MockDependencyTest.java
+++ src/test/java/com/nadeem/app/compare/mock/jmockit/case1/MockDependencyTest.java
@@ -5,6 +5,8 @@ import static org.fest.assertions.api.Assertions.assertThat;
 import java.io.IOException;
 
 import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 import mockit.Mocked;
 
 import org.junit.Test;
@@ -14,9 +16,8 @@ import com.nadeem.app.compare.mock.case1.MockDependency;
 
 public class MockDependencyTest {
 
-	
 	@Test
-    public void shouldMockDependency(@Mocked final Dependency dependency) throws IOException {
+    public void shouldMockDependencyBehaviorBased(@Mocked final Dependency dependency) throws IOException {
 		MockDependency mockDependency = new MockDependency(dependency);
         new Expectations() {{
             dependency.message();
@@ -24,4 +25,21 @@ public class MockDependencyTest {
         }};
         assertThat(mockDependency.go()).isEqualTo("I am mocked!");
     }
+
+	@Test
+    public void shouldMockDependencyStateBased(@Mocked final Dependency dependency) throws IOException {
+
+		new MockedDependency();
+		MockDependency mockDependency = new MockDependency(dependency);
+        
+        assertThat(mockDependency.go()).isEqualTo("I am mocked!");
+    }
+
+	private static class MockedDependency extends MockUp<Dependency> {
+
+		@Mock
+		public String message() {
+			return "I am mocked!";
+		}
+	}
 }
